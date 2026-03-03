e07f33bb2d8308d21fac6cca4e95b764c36f96cb
Sara Zuluaga <sarazuluagatrujillo@gmail.com>
2026-03-03 10:47:11 -0500

    primera parte tutorial 3B

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,5 @@ Route::get('/products/{id}', 'App\Http\Controllers\ProductController@show')->nam
 Route::get('/cart', 'App\Http\Controllers\CartController@index')->name("cart.index"); 
 Route::get('/cart/add/{id}', 'App\Http\Controllers\CartController@add')->name("cart.add"); 
 Route::get('/cart/removeAll/', 'App\Http\Controllers\CartController@removeAll')->name("cart.removeAll"); 
+Route::get('/image', 'App\Http\Controllers\ImageController@index')->name("image.index"); 
+Route::post('/image/save', 'App\Http\Controllers\ImageController@save')->name("image.save");
